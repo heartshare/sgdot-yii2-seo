3c808328486295449256e97ba181e28ee2e52d8d
SergeiGulin <gulin-sergei@ya.ru>
2014-09-08 17:28:44 +0400

    Update Seo.php

--- vendor/sgdot/Seo.php
+++ vendor/sgdot/Seo.php
@@ -3,8 +3,38 @@
 namespace sgdot\seo;
 
 use yii\base\Widget;
+use yii\helpers\Url;
+use yii\helpers\Html;
+use Yii;
 
 class Seo extends Widget {
+    public $modelClass;
+    
     public function run() {
+        parent::run();
+        /* @var $view \yii\web\View */
+        $view = \Yii::$app->getView();
+        $seo = $this->getSeo();
+        if (is_null($seo)) {
+            echo Html::tag('title', $view->title) . PHP_EOL;
+        } else {
+            echo Html::tag('title', $seo->title) . PHP_EOL;
+            $view->registerMetaTag([
+                'name' => 'keywords',
+                'content' => $seo->keywords,
+            ]);
+            $view->registerMetaTag([
+                'name' => 'description',
+                'content' => $seo->description,
+            ]);
+        }
+    }
+    public function getSeo() {
+        /* @var $request yii\web\Request */
+        $request = Yii::$app->request;
+        $host = $request->hostInfo;
+        $currentUrl = strtr(Url::canonical(), [$host => '']);
+        $class = $this->modelClass;
+        return $class::findOne(['url' => $currentUrl]);
     }
 }
